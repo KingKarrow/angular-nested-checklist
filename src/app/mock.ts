07eccfa59ad0b68594a45c81ae9b974aaabf9b29
karrow <mgmckean@gmail.com>
2024-02-27 02:15:00 -0500

    nested checklist component tests

--- src/app/mock.ts
+++ src/app/mock.ts
@@ -2,14 +2,60 @@
 
 export const mockChecklist = [
   {
-    "name": "Big task",
-    "checked": false
+    name: 'Big task',
+    checked: false
   }
 ];
 
 export const mockConfig = {
-  'checklist': mockChecklist
+  checklist: mockChecklist
 };
+
+export const mockChecklistComplex = [
+  { name: '1.0.0.0', checked: false, children: [
+    { name: '1.1.0.0', checked: false, children: [
+      { name: '1.1.1.0', checked: false, children: [
+        { name: '1.1.1.1', checked: false },
+        { name: '1.1.1.2', checked: false },
+        { name: '1.1.1.3', checked: false }
+      ]},
+      { name: '1.1.2.0', checked: false }
+    ]},
+    { name: '1.2.0.0', checked: false, children: [
+      { name: '1.2.1.0', checked: false },
+      { name: '1.2.2.0', checked: false },
+      { name: '1.2.3.0', checked: false, children: [
+        { name: '1.2.3.1', checked: false },
+        { name: '1.2.3.2', checked: false }
+      ]},
+    ]}
+  ]}
+];
+
+export const mockConfigComplex = {
+  checklist: mockChecklistComplex
+};
+
+export const mockChecklistComplexTrue = [
+  { name: '1.0.0.0', checked: true, children: [
+    { name: '1.1.0.0', checked: true, children: [
+      { name: '1.1.1.0', checked: true, children: [
+        { name: '1.1.1.1', checked: true },
+        { name: '1.1.1.2', checked: true },
+        { name: '1.1.1.3', checked: true }
+      ]},
+      { name: '1.1.2.0', checked: true }
+    ]},
+    { name: '1.2.0.0', checked: true, children: [
+      { name: '1.2.1.0', checked: true },
+      { name: '1.2.2.0', checked: true },
+      { name: '1.2.3.0', checked: true, children: [
+        { name: '1.2.3.1', checked: true },
+        { name: '1.2.3.2', checked: true }
+      ]},
+    ]}
+  ]}
+];
 
 // export const mockHttpClient = {
 //   get: () => { return of(mockConfig) }
--- src/app/modules/nested-checklist/components/nested-checklist/nested-checklist.component.spec.ts
+++ src/app/modules/nested-checklist/components/nested-checklist/nested-checklist.component.spec.ts
@@ -1,10 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
 
 import { NestedChecklistComponent } from './nested-checklist.component';
 import { MatCheckbox, MatCheckboxModule } from '@angular/material/checkbox';
-import { FormBuilder, FormsModule } from '@angular/forms';
-import { NestedChecklistNode } from '../../models/nested-checklist-node';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { of } from 'rxjs';
+import { mockChecklist, mockChecklistComplex, mockChecklistComplexTrue } from 'src/app/mock';
+import { NestedChecklistNodeComponent } from '../nested-checklist-node/nested-checklist-node.component';
 
 describe('NestedChecklistComponent', () => {
   let component: NestedChecklistComponent;
@@ -12,19 +13,93 @@ describe('NestedChecklistComponent', () => {
   let mockMatCheckbox: jasmine.SpyObj<MatCheckbox>;
 
   beforeEach(async () => {
-    // await TestBed.configureTestingModule({
-    //   declarations: [ NestedChecklistComponent ],
-    //   imports: [ FormsModule, MatCheckboxModule ]
-    // })
-    // .compileComponents();
+    await TestBed.configureTestingModule({
+      declarations: [ NestedChecklistComponent, NestedChecklistNodeComponent ],
+      imports: [ FormsModule, ReactiveFormsModule, MatCheckboxModule ]
+    })
+    .compileComponents();
 
-    // fixture = TestBed.createComponent(NestedChecklistComponent);
-    component = new NestedChecklistComponent(new FormBuilder());
-    component.config = of([ new NestedChecklistNode('test', false) ]);
-    // fixture.detectChanges();
+    fixture = TestBed.createComponent(NestedChecklistComponent);
+    component = fixture.componentInstance;
+    // component = new NestedChecklistComponent(new FormBuilder());
+    
   });
-
+  
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Using SIMPLE checklist', () => {
+    beforeEach(async () => {
+      component.config = of(mockChecklist);
+      fixture.detectChanges();
+    });
+
+    it('should populate checklistData and formArray', (done) => {
+      component.config.subscribe(() => {
+        expect(component.checklistData).toEqual(mockChecklist);
+        expect(component.formArray.length).toBe(1);
+        done();
+      });
+    });
+  
+    it('should update node.checked on control change', (done) => {
+      component.config.subscribe(() => {
+        const control = component.formArray.at(0);
+        control.valueChanges.subscribe(() => {
+          expect(component.checklistData[0].checked).toBe(true);
+          done();
+        });
+        control.setValue(true);
+      });
+    });
+  });
+
+  describe('Using COMPLEX checklist', () => {
+    beforeEach(async () => {
+      component.config = of(mockChecklistComplex);
+      fixture.detectChanges();
+    });
+
+    it('should populate COMPLEX checklistData and formArray', (done) => {
+      component.config.subscribe(() => {
+        expect(component.checklistData).toEqual(mockChecklistComplex);
+        expect(component.formArray.length).toBe(13);
+        done();
+      });
+    });
+
+    it('should uncheck all parents if a child is unchecked', (done) => {
+      component.config = of(mockChecklistComplexTrue);
+      fixture.detectChanges();
+      component.config.subscribe(() => {
+        const control = component.formArray.at(4);
+        control.valueChanges.subscribe(() => {
+          expect(component.checklistFlat[4].checked).toBe(false);
+          expect(component.checklistFlat[2].checked).toBe(false);
+          expect(component.checklistFlat[1].checked).toBe(false);
+          expect(component.checklistFlat[0].checked).toBe(false);
+          done();
+        });
+        control.setValue(false);
+      });
+    });
+
+    it('should check parent if all children checked', (done) => {
+      component.config.subscribe(() => {
+        const child1Control = component.formArray.at(3);
+        const child2Control = component.formArray.at(4);
+        const child3Control = component.formArray.at(5);
+        child3Control.valueChanges.subscribe(() => {
+          expect(component.checklistFlat[2].checked).toBe(true);
+          expect(component.checklistFlat[1].checked).toBe(false);
+          expect(component.checklistFlat[0].checked).toBe(false);
+          done();
+        });
+        child1Control.setValue(true);
+        child2Control.setValue(true);
+        child3Control.setValue(true);
+      });
+    });
   });
 });
